8138935ff1836acb3e4779ba6ad7f9b9bc6373c4
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-04 15:49:58 +0530

    applying js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const APIURL = 'https://api/.github.com/users/'
+
+const form = document.getElementById('form')
+
+getUser('bradtraversy')
+
+async function getUser(username) {
+  try {
+    const {data} = await axios(APIURL + username)
+
+    console.log(data)
+  } catch(err) {
+    console.log(err)
+  }
+
+}
+
+form.
